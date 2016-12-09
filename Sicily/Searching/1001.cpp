ce3cdb24ca8644a389bd9c6e07f8faadf2ce1414
wubowen <bowen.wu@163.com>
2016-12-09 15:35:56 +0800

    Finish 1001 binarySearch

--- Sicily/Searching/1001.cpp
+++ Sicily/Searching/1001.cpp
@@ -5,26 +5,32 @@ int binSearch(const int s[], const int size, const int target) {
     int position = size / 2;
     int begin = 0;
     int end = size;
-    while (end > begin) {
-        cout << "position : " << position << " begin : " << begin << " end : " << end << endl;
+    while (end - begin > 1) {
         position = (begin + end) / 2;
-        if (target <= s[position]) {
+        // cout << "position : " << position << " begin : " << begin << " end : " << end << endl;
+        if (target > s[position]) {
             begin = position;
             continue;
-        } else if (target > s[position]) {
+        } else if (target < s[position]) {
             end = position;
+        } else if (target == s[position]) {
+            break;
         }
     }
-    if (target == s[position])
-        return position;
-    else
+    if (target == s[position]) {
+        for (int i = position; i + 1 < size; ++i) {
+            if (s[i + 1] != target)
+                return i;
+        }
+        return size - 1;
+    } else
         return -1;
 }
 
 int main() {
-    int s[8] = {0,1,1,3,3,3,6,6};
+    int s[10] = {0,1,1,2,3,3,6,6, 10, 10};
 
-cout << binSearch(s,8,3) << endl;  //输出5
+cout << binSearch(s,10,3) << endl;  //输出5
 
-cout << binSearch(s,8,4) << endl;  //输出-1
+cout << binSearch(s,10,10) << endl;  //输出-1
 }
